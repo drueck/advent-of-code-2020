ce76fefc11647e09c6bba6ae0291838556bfe766
David Rueck <drueck@gmail.com>
2020-12-06 11:46:48 -0800

    Eliminate impossible entries before iterating through combinations
    This speeds up the process for larger group sizes significantly!

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -34,11 +34,23 @@ fn main() {
 
     let file = File::open(input_file).expect("no such file");
     let buf = BufReader::new(file);
-    let entries: Vec<u32> = buf
+    let mut entries: Vec<u32> = buf
         .lines()
         .map(|l| l.expect("could not parse line"))
         .map(|s| s.parse::<u32>().expect("entry was not an int"))
-        .collect();
+        .collect::<Vec<u32>>();
+
+    let initial_entries_count = entries.len();
+
+    entries.sort();
+    remove_impossible_entries(&mut entries, group_size);
+
+    println!();
+    println!("Found {} entries", initial_entries_count);
+    println!(
+        "Eliminated {} entries that could not be part of the solution",
+        initial_entries_count - entries.len()
+    );
 
     let combinations = Combinations::new(entries, group_size);
     let mut iterations = 0;
@@ -54,6 +66,13 @@ fn main() {
     display_failure_message(group_size, iterations);
 }
 
+fn remove_impossible_entries(entries: &mut Vec<u32>, group_size: usize) {
+    let smallest_entries = entries[0..(group_size - 1)].to_vec();
+    let sum_of_smallest = smallest_entries.iter().sum::<u32>();
+
+    entries.retain(|&entry| entry + sum_of_smallest <= 2020)
+}
+
 fn display_success_message(combination: Vec<u32>, iterations: usize) {
     let numbers = combination
         .iter()
@@ -62,6 +81,7 @@ fn display_success_message(combination: Vec<u32>, iterations: usize) {
 
     let product = combination.iter().product::<u32>();
     println!();
+    println!("Success!");
     println!("{} = 2020", numbers.join(" + "));
     println!("{} = {}", numbers.join(" * "), product);
     println!();
@@ -70,6 +90,7 @@ fn display_success_message(combination: Vec<u32>, iterations: usize) {
 
 fn display_failure_message(group_size: usize, iterations: usize) {
     println!();
+    println!("Bummer!");
     println!(
         "Did not find any combinations of {} entries that totalled 2020",
         group_size
